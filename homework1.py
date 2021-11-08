3899c0263b67fdaebde0d2b7cf225ff18ff664c1
HSEhomework <31905166+HSEhomework@users.noreply.github.com>
2021-11-09 01:10:09 +0300

    Added comments and mark

--- homework1.py
+++ homework1.py
@@ -10,14 +10,21 @@ parser.add_argument("--rename", help="Change file name", default="")
 parser.add_argument("-m", "--mtime", help="Print modification time", action="store_true")
 args = parser.parse_args()
 print("Args", args)
+# What if file not probided to arguments?  George
 print("filename", args.file)
 if args.ls:
     print("files in a directory:", ', '.join(os.listdir()))
 if args.mtime:
+    # time format? George
     st = os.stat(args.file)
     print("last modified: %s" % time.ctime(st.st_mtime))
 if args.size:
     stats = os.stat(args.file)
+    # File size in Mb, not Kb please. st_size gives bytes! George
     print("file size in Megabytes", stats.st_size/1024)
 if args.rename != "":
+    # What if file not probided to arguments? George
+    # What if file does not exist? George
     os.rename(args.file, args.rename)
+    
+# Good solution. For now 3/5 points. Please support time formatting options + fix comments to improve
